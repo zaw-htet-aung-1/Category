6d7f9a0611ba84e1080f7117a9b8620967ee7fed
Thet Minn Htun <59432845+thetminnhtun-scm@users.noreply.github.com>
2022-06-19 18:20:03 +0630

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 
-// Route::get('/posts', [PostController::class, 'index']);
-// Route::get('/posts/create', [PostController::class, 'create']);
-// Route::post('/posts', [PostController::class, 'store']);
-// Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
-// Route::put('/posts/{id}', [PostController::class, 'update']);
-// Route::patch('/posts/{id}', [PostController::class, 'update']);
-// Route::get('/posts/{id}', [PostController::class, 'show']);
-// Route::delete('/posts/{id}', [PostController::class, 'destroy']);
-
 Route::get('/', [PostController::class, 'index']);
-Route::resource('posts', PostController::class);
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('myauth');
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::patch('/posts/{id}', [PostController::class, 'update']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+
+// Route::resource('posts', PostController::class);
 
 Route::get('register', [RegisterController::class, 'create']);
 Route::post('register', [RegisterController::class, 'store']);
